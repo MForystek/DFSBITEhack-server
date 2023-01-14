5464685f62148959f82096f9377f5044b2bb2b28
xsalonx <lukasz.dubiel3@gmail.com>
2023-01-14 23:49:38 +0100

    oauth works

--- src/main/resources/db/create-tables.sql
+++ src/main/resources/db/create-tables.sql
@@ -25,20 +25,20 @@ CREATE DATABASE "kgex-db"
 -- DROP TABLE IF EXISTS public.users CASCADE;
 CREATE TABLE public.users (
 	id serial NOT NULL,
-	nick text NOT NULL,
+	username text NOT NULL,
 	password text NOT NULL,
 	email text NOT NULL,
-	creation_date date NOT NULL,
-	verification_date date,
+	creation_at date,
+	verification_at date,
 	icon_path text,
 	verification_method_id int4,
 	CONSTRAINT user_pk PRIMARY KEY (id),
-	CONSTRAINT login_uq UNIQUE (nick),
+	CONSTRAINT login_uq UNIQUE (username),
 	CONSTRAINT email_uq UNIQUE (email),
 	CONSTRAINT icon_path_uq UNIQUE (icon_path)
 );
 -- ddl-end --
-COMMENT ON COLUMN public.users.verification_date IS E'if null uesr is unverified';
+COMMENT ON COLUMN public.users.verification_at IS E'if null uesr is unverified';
 -- ddl-end --
 COMMENT ON COLUMN public.users.icon_path IS E'path';
 -- ddl-end --
@@ -81,6 +81,28 @@ CREATE TABLE public.verification_method_dict (
 ALTER TABLE public.verification_method_dict OWNER TO postgres;
 -- ddl-end --
 
+-- object: public.roles | type: TABLE --
+-- DROP TABLE IF EXISTS public.roles CASCADE;
+CREATE TABLE public.roles (
+	id serial,
+	name varchar NOT NULL
+
+);
+-- ddl-end --
+ALTER TABLE public.roles OWNER TO postgres;
+-- ddl-end --
+
+-- object: public.user_roles | type: TABLE --
+-- DROP TABLE IF EXISTS public.user_roles CASCADE;
+CREATE TABLE public.user_roles (
+	user_id int4 NOT NULL,
+	role_id int4 NOT NULL,
+	CONSTRAINT user_roles_pk PRIMARY KEY (user_id,role_id)
+);
+-- ddl-end --
+ALTER TABLE public.user_roles OWNER TO postgres;
+-- ddl-end --
+
 -- object: verification_method_fk | type: CONSTRAINT --
 -- ALTER TABLE public.users DROP CONSTRAINT IF EXISTS verification_method_fk CASCADE;
 ALTER TABLE public.users ADD CONSTRAINT verification_method_fk FOREIGN KEY (verification_method_id)
@@ -99,6 +121,20 @@ ON DELETE CASCADE ON UPDATE CASCADE;
 -- ALTER TABLE public.users_tags DROP CONSTRAINT IF EXISTS tag_id_fk CASCADE;
 ALTER TABLE public.users_tags ADD CONSTRAINT tag_id_fk FOREIGN KEY (tag_id)
 REFERENCES public.tags (id) MATCH SIMPLE
+ON DELETE CASCADE ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: user_id_fk | type: CONSTRAINT --
+-- ALTER TABLE public.user_roles DROP CONSTRAINT IF EXISTS user_id_fk CASCADE;
+ALTER TABLE public.user_roles ADD CONSTRAINT user_id_fk FOREIGN KEY (user_id)
+REFERENCES public.users (id) MATCH SIMPLE
+ON DELETE CASCADE ON UPDATE CASCADE;
+-- ddl-end --
+
+-- object: role_id_fk | type: CONSTRAINT --
+-- ALTER TABLE public.user_roles DROP CONSTRAINT IF EXISTS role_id_fk CASCADE;
+ALTER TABLE public.user_roles ADD CONSTRAINT role_id_fk FOREIGN KEY (role_id)
+REFERENCES public.roles (id) MATCH SIMPLE
 ON DELETE CASCADE ON UPDATE CASCADE;
 -- ddl-end --
 
